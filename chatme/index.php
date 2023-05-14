bf183da1a4fccf0affa366c666a74da3cfd6a09e
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-05-14 15:41:44 -0700

    Create index.php

---
+++ chatme/index.php
@@ -0,0 +1,58 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['login_user'])) {
+	
+	header('location: ./../');
+	exit;
+}
+
+include('./../database/database.php');
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Chatme - Chats</title>
+	<link rel="stylesheet" type="text/css" href="./../others/bootstrap.css">
+	<style type="text/css">
+		body {
+			margin-top: 150px;
+			margin-left: 30px;
+		}
+	</style>
+</head>
+<body>
+
+	<div>
+		<a href="logout.php" class="btn btn-warning">Logout</a>
+	</div>
+
+	<?php
+
+	$currentuser = $_SESSION['userid'];
+
+	$query = "SELECT * FROM user WHERE id <> '$currentuser'";
+	$result = mysqli_query($connect, $query);
+
+	if (mysqli_num_rows($result) > 0) {
+		while($rows = mysqli_fetch_assoc($result)) {
+
+		?>
+
+		<p><a href="#"><?php echo $rows['id'];?>&nbsp;&nbsp;<?php echo $rows['username'];?></a></p>
+
+		<?php
+	}
+}
+
+
+	?>
+
+</body>
+</html>
