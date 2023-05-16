aa2fa91bb196ba46a7c39a4e421c7b057c3cd255
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-05-16 14:09:26 -0700

    Update chatme.sql

--- database/chatme.sql
+++ database/chatme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2023 at 11:40 PM
+-- Generation Time: May 16, 2023 at 11:07 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -36,6 +36,22 @@ CREATE TABLE `messages` (
   `time` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `readerid`, `senderid`, `message`, `date`, `time`) VALUES
+(1, 5, 2, 'Hello!', '15/05/2023', '10:25:48pm'),
+(2, 5, 2, 'Hello!', '15/05/2023', '10:27:30pm'),
+(3, 5, 2, 'Hi', '15/05/2023', '10:33:03pm'),
+(4, 2, 5, 'Hello Aggrey. How Are You?', '15/05/2023', '10:35:23pm'),
+(5, 3, 2, 'Hello!', '16/05/2023', '02:16:01am'),
+(6, 3, 5, 'Hello', '16/05/2023', '02:49:16am'),
+(7, 2, 2, 'Hello too...', '16/05/2023', '02:52:05am'),
+(8, 3, 5, 'Hello', '16/05/2023', '03:27:31am'),
+(9, 4, 2, 'Hello! Habari yako...', '16/05/2023', '03:34:18am'),
+(10, 3, 2, 'Hi', '16/05/2023', '03:50:29am');
+
 -- --------------------------------------------------------
 
 --
@@ -61,6 +77,16 @@ CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(2, 'aggrey@jrey.co.ke', '$2y$10$tz/8E0FwUPGk1Lnf3SgrA.AsnOFJ2oH18UKhL.9VfrfoCYeRVi5BC'),
+(3, 'kiprop@jrey.co.ke', '$2y$10$mjJD2OlS05wKht910dqZVeNn9ucOXkajdzoUt9hA7gfV8126WSrxK'),
+(4, 'aggreykiprop60@gmail.com', '$2y$10$IAsYLDbrby4IHHmOUuAfW.Wd7/Aq8rt2hyY/NRodOKsukMBl1unDC'),
+(5, 'test456@developer.com', '$2y$10$Uxzwptf5yLBIzfy0EssvF..dMZ95QmEuJZeh/McuDVCa0bpF.Lmyq');
 
 --
 -- Indexes for dumped tables
@@ -92,7 +118,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `readmessages`
@@ -104,7 +130,7 @@ ALTER TABLE `readmessages`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
