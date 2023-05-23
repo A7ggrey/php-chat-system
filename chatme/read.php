51b8332122f55ea990c758d38256555d2f4fb0d2
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-05-22 20:36:03 -0700

    Update read.php

--- chatme/read.php
+++ chatme/read.php
@@ -63,10 +63,10 @@ $reciever_name = $select_user_rows['username'];
 
 						    if ($sender == $rows_selected['senderid']) {
 						    	
-						    	echo '<div class="input-group mb-3"><span style="text-align: right;">' .$rows_selected['message']. '<span style="color: darkgrey; font-size: 12px; margin-top: 7px;">' .$rows_selected['time']. ' ' .$rows_selected['date']. '</span></span></div>';
+						    	echo '<div class="input-group mb-3"><span style="text-align: right;">' .$rows_selected['message']. ' <span style="color: darkgrey; font-size: 9px; margin-top: 7px;">' .$rows_selected['time']. ' ' .$rows_selected['date']. ' </span><span style="color: blue; font-size: 9px; margin-top: 7px;">send</span></span></div>';
 						    } else {
 
-						    	echo '<div class="input-group mb-3"><span style="text-align: left;">' .$rows_selected['message']. '<span style="color: hotpink; font-size: 12px; margin-top: 7px;">' .$rows_selected['time']. ' ' .$rows_selected['date']. '</span></div>';
+						    	echo '<div class="input-group mb-3"><span style="text-align: left;">' .$rows_selected['message']. ' <span style="color: hotpink; font-size: 9px; margin-top: 7px;">' .$rows_selected['time']. ' ' .$rows_selected['date']. ' </span> <span style="color: blue; font-size: 9px; margin-top: 7px;"> recieve</span></span></div>';
 						    }
 						?>
 					</p>
