4295527704752387bc9b5b533afa3f5595586cb9
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-05-22 19:06:19 -0700

    Update read.php

--- chatme/read.php
+++ chatme/read.php
@@ -64,6 +64,35 @@ $sender = $_SESSION['userid'];
 		?>
 	</div>
 
+	<div class="login-box">
+		<div class="card card-outline card-primary">
+			<div class="card-header text-center">
+				<h4 class="h4">Login Here!</h4>
+			</div>
+			<form method="POST" action="process.php">
+				<div class="card-body">
+				<div class="input-group mb-3">
+					<input type="text" name="email" class="form-control" placeholder="Email">
+				</div>
+
+				<div class="input-group mb-3">
+					<input type="password" name="password" class="form-control" placeholder="Password">
+				</div>
+
+				<div class="input-group mb-3">
+					<input type="submit" name="login" value="Login" class="btn btn-success">
+				</div>
+				<a href="register.html" class="alert-link text-decoration-none">Create an account</a>
+			</div>
+			</form>
+
+			<div class="card-footer">
+				<p><span style='color: orange;'>username 1</span>: aggrey@jrey.co.ke || <span style='color: orange;'>password 1</span>: aggrey123</p>
+				<p><span style='color: orange;'>username 2</span>: kiprop@jrey.co.ke || <span style='color: orange;'>password 2</span>: kiprop123</p>
+			</div>
+		</div>
+	</div>
+
 	<form method="POST" action="send-message.php">
 		<div class="btn-group">
 			<input type="hidden" name="recieverid" value="<?php echo $user;?>">
