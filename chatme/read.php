82bc09bb0f0d26716de7a9fe6da99de206088736
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-05-25 22:21:28 -0700

    Update read.php

--- chatme/read.php
+++ chatme/read.php
@@ -63,7 +63,30 @@ $reciever_name = $select_user_rows['full_name'];
         line-height: 200px; /* Create scrollbar to test positioning */
     }
 
-    @media only screen and (max-width: 600px) {
+    .sender-div {
+    	width: 250px;
+    	float: right;
+    	background-color: lightblue;
+    }
+
+    .sender-span-1 {
+    	margin-left: 7px;
+    }
+
+    .sender-span-2 {
+    	 color: black;
+    	 font-size: 9px;
+    	 margin-top: 7px;
+    	 margin-left: 100px;
+    }
+
+    .sender-span-3 {
+    	 color: blue;
+    	 font-size: 9px;
+    	 margin-top: 7px;
+    }
+
+    @media only screen and (max-device-width: 480px) {
     	/* Add some padding on document's body to prevent the content
     to go underneath the header and footer */
     body{        
@@ -80,14 +103,16 @@ $reciever_name = $select_user_rows['full_name'];
         background: brown;
         padding: 10px 0;
         color: #fff;
-        height: 350px;
-        background-color: blue;
+        height: 100px;
+        font-size: 35px;
     }
     .fixed-header{
         top: 0;
     }
     .fixed-footer{
         bottom: 0;
+        padding-left: 25px;
+        padding-top: 25px;
     }    
     /* Some more styles to beutify this example */
     nav a{
@@ -98,6 +123,79 @@ $reciever_name = $select_user_rows['full_name'];
     }
     .container p{
         line-height: 200px; /* Create scrollbar to test positioning */
+    }
+
+    .messages {
+    	margin-top: 90px;
+    }
+    .form-control {
+    	font-size: 40px;
+    	border-radius: 10px;
+    }
+    #btn {
+    	font-size: 40px;
+    	background-color: blue;
+    	color: white;
+    	border-radius: 12px;
+    }
+    .sender-div {
+    	width: 350px;
+    	float: right;
+    	background-color: lightblue;
+    	padding-top: 10px;
+    	padding-left: 4px;
+    	margin: 10px;
+    	margin-bottom: 20px;
+    }
+
+    .sender-span-1 {
+    	margin-left: 7px;
+    	font-size: 30px;
+    }
+
+    .sender-span-2 {
+    	 color: black;
+    	 font-size: 9px;
+    	 margin-top: 7px;
+    	 margin-left: 100px;
+    	 font-size: 18px;
+    }
+
+    .sender-span-3 {
+    	 color: blue;
+    	 font-size: 9px;
+    	 margin-top: 7px;
+    	 font-size: 18px;
+    }
+
+    .receiver-div {
+    	width: 350px;
+    	float: left;
+    	background-color: lightgreen;
+    	padding-top: 10px;
+    	padding-left: 4px;
+    	margin: 10px;
+    	margin-bottom: 20px;
+    }
+
+    .receiver-span-1 {
+    	margin-left: 7px;
+    	font-size: 30px;
+    }
+
+    .receiver-span-2 {
+    	 color: black;
+    	 font-size: 9px;
+    	 margin-top: 7px;
+    	 margin-left: 100px;
+    	 font-size: 18px;
+    }
+
+    .receiver-span-3 {
+    	 color: blue;
+    	 font-size: 9px;
+    	 margin-top: 7px;
+    	 font-size: 18px;
     }
     }
 </style>
@@ -110,7 +208,7 @@ $reciever_name = $select_user_rows['full_name'];
             </nav>
         </div>
     </div>
-    <div class="container">
+    <div class="container messages">
     	<?php
 
 		$select_message_query = "SELECT * FROM messages WHERE readerid = '$user' AND senderid = '$sender' OR readerid = '$sender' AND senderid = '$user'";
@@ -129,12 +227,12 @@ $reciever_name = $select_user_rows['full_name'];
 
 						    if ($sender == $rows_selected['senderid']) {
 						    	
-						    	echo '<div class="input-group mb-3" style="width: 250px; float: right; background-color: lightblue;"><span style="margin-left: 7px;">' .$rows_selected['message']. ' <br><span style="color: black; font-size: 9px; margin-top: 7px; margin-left: 100px;">' .$rows_selected['time']. ' ' .$rows_selected['date']. ' </span><span style="color: blue; font-size: 9px; margin-top: 7px;">send</span></span></div><br><br><br>';
+						    	echo '<div class="input-group mb-3 sender-div"><span class="sender-span-1">' .$rows_selected['message']. ' <br><span class="sender-span-2">' .$rows_selected['time']. ' ' .$rows_selected['date']. ' </span><span class="sender-span-3">send</span></span></div>';
 						    } else {
 
 						    	//echo '<div class="input-group mb-3 bg-warning" style="width: 250px; float: left;"><span style="margin-left: 5px;">' .$rows_selected['message']. ' <br><span style="color: black; font-size: 9px; margin-top: 7px; margin-left: 100px;">' .$rows_selected['time']. ' ' .$rows_selected['date']. ' </span><span style="color: blue; font-size: 9px; margin-top: 7px;">send</span></span></div>';
 
-						    	echo '<div class="input-group mb-3 bg-success" style="width: 250px; float: left; background-color: lightgreen;><span style="margin-left: 7px;">' .$rows_selected['message']. ' <span style="color: hotpink;  font-size: 9px; margin-top: 7px; margin-left: 100px;">' .$rows_selected['time']. ' ' .$rows_selected['date']. ' </span><span style="color: blue; font-size: 9px; margin-top: 7px;"> recieve</span></span></div><br><br><br>';
+						    	echo '<div class="input-group mb-3 bg-success receiver-div"><span class="receiver-span-1">' .$rows_selected['message']. ' <span class="receiver-span-2">' .$rows_selected['time']. ' ' .$rows_selected['date']. ' </span><span class="receiver-span-3"> recieve</span></span></div>';
 						    }
 						?>
 					</p>
@@ -156,7 +254,7 @@ $reciever_name = $select_user_rows['full_name'];
 		    <div class="btn-group">
 			        <input type="hidden" style="margin-left: 5px;" name="recieverid" value="<?php echo $user;?>">
 			        <input type="text"  style="margin-left: 5px;"name="message" class="form-control" placeholder="Write message..." required>
-		        <input type="submit" style="margin-left: 5px;" name="send" value="send" class="btn btn-info">
+		        <input type="submit" style="margin-left: 5px;" name="send" value="send" class="btn btn-info" id="btn">
 		    </div>
 	    </form>        
     </div>
