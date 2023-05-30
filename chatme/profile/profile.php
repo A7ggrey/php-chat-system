b6d6c38688b74b5feeff2f4d5e049fb4671b0946
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-05-30 21:48:00 +0300

    Update profile.php

--- chatme/profile/profile.php
+++ chatme/profile/profile.php
@@ -10,21 +10,54 @@ if (!isset($_SESSION['login_user'])) {
 
 include('./../../database/database.php');
 
-$current_user = $_SESSION['userid'];
-
-$select_profile = "SELECT * FROM user WHERE id = '$current_user'";
-$select_profile_result = mysqli_query($connect, $select_profile);
-
-if (mysqli_num_rows($select_profile_result) > 0) {
+if (isset($_GET['opid'])) {
 	
-	$select_profile_rows = mysqli_fetch_assoc($select_profile_result);
+	$follow_me = $_GET['opid'];
+} else {
 
-	$current_user_name = $select_profile_rows['full_name'];
-	$current_user_username = $select_profile_rows['username'];
-	$current_user_profile = $select_profile_rows['profile_photo'];
-	$current_user_verify = $select_profile_rows['verified'];
+	$follow_me = $_SESSION['userid'];
 }
 
+if (!isset($_GET['opid'])) {
+	
+	$current_user = $_SESSION['userid'];
+
+    $select_profile = "SELECT * FROM user WHERE id = '$current_user'";
+    $select_profile_result = mysqli_query($connect, $select_profile);
+
+    if (mysqli_num_rows($select_profile_result) > 0) {
+	
+	    $select_profile_rows = mysqli_fetch_assoc($select_profile_result);
+
+	    $current_user_name = $select_profile_rows['full_name'];
+	    $current_user_username = $select_profile_rows['username'];
+	    $current_user_profile = $select_profile_rows['profile_photo'];
+	    $current_user_verify = $select_profile_rows['verified'];
+} else {
+
+	//echo "<script>history.back(-1);</script>";
+}
+
+} else {
+
+	$opid = $_GET['opid'];
+
+	$select_profile = "SELECT * FROM user WHERE id = '$opid'";
+    $select_profile_result = mysqli_query($connect, $select_profile);
+
+    if (mysqli_num_rows($select_profile_result) > 0) {
+	
+	    $select_profile_rows = mysqli_fetch_assoc($select_profile_result);
+
+	    $current_user_name = $select_profile_rows['full_name'];
+	    $current_user_username = $select_profile_rows['username'];
+	    $current_user_profile = $select_profile_rows['profile_photo'];
+	    $current_user_verify = $select_profile_rows['verified'];
+} else {
+
+	//echo "<script>history.back(-1);</script>";
+}
+}
 ?>
 
 <!DOCTYPE html>
@@ -54,6 +87,26 @@ if (mysqli_num_rows($select_profile_result) > 0) {
 		
 		<p>
 			<?php echo $current_user_username;?>
+		</p>
+
+		<p>
+			<?php
+
+			$select_followers = "SELECT * FROM followers WHERE my_id = '$follow_me'";
+			$select_followers_result = mysqli_query($connect, $select_followers);
+
+			$count_followers = mysqli_num_rows($select_followers_result);
+
+			echo "Followed By <span style='color: blue;'>" .$count_followers. "</span> User";
+
+			?>
+		</p>
+
+		<p>
+			<form method="POST" action="follower.php">
+				<input type="hidden" name="follow_id" value="<?php echo $follow_me;?>">
+				<input type="submit" name="follower_btn" value="Follow">
+			</form>
 		</p>
 		
 	</div>
