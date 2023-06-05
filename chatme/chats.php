a52a7157e6aad7c70b985d8a1696f612e248d0a2
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-06-05 10:52:24 +0300

    Update chats.php

--- chatme/chats.php
+++ chatme/chats.php
@@ -3,221 +3,308 @@
 session_start();
 
 if (!isset($_SESSION['login_user'])) {
-	
-	header('location: ./../');
-	exit;
+  
+  header('location: ./../');
+  exit;
 }
 
 include('./../database/database.php');
 
-
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Chatme - Start a Chat</title>
-	<link rel="stylesheet" type="text/css" href="./../others/bootstrap.css">
-	<style type="text/css">
-		body {
-			margin-top: 150px;
-			margin-left: 30px;
-		}
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Chat Me | Chats</title>
 
-		button {
-			margin-top: 20px;
-			border-top: none;
-			border-bottom: none;
-			border-left: none;
-			border-right: none;
-			background-color: white;
-		}
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="./../plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="./../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="./../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="./../plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./../dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="./../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="./../plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="./../plugins/summernote/summernote-bs4.min.css">
+  <style type="text/css">
+    #content_section {
+      margin-top: 110px;
+    }
+    .main_div {
+      margin: 0 auto;
+      width: 60%;
+    }
 
-		.verified {
-			width: 20px;
-			height: 20px;
-			border-radius: 100%;
-			border-style: double;
-			border-color: rgb(160, 238, 95);
-		}
+    @media (max-width: 480px) {
 
-		.profile_user {
-			width: 40px;
-			height: 40px;
-			border-radius: 100%;
-			border-style: double;
-			border-color: rgb(77, 46, 161);
-		}
-
-	</style>
+      #content_section {
+        margin-top: 100px;
+      }
+      .main_div {
+      margin: 0 auto;
+      width: 95%;
+    }
+    }
+  </style>
 </head>
-<body>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="main_div">
 
-	<div>
-		<a href="logout.php" class="btn btn-warning">Logout</a><br>
-		<form method="GET" action="">
-			<input type="text" name="search" placeholder="Search Username" required>
-			<input type="submit" value="Search">
-		</form>
-	</div>
 
-	<?php
+  <!-- Main content -->
+    <section class="content" id="content_section">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
 
-	if (!isset($_GET['search'])) {
-		
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-12 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <!-- DIRECT CHAT -->
+            <div class="card direct-chat direct-chat-primary">
+              <div class="card-header">
+                <h3 class="card-title">Chats - <a href="./new_chats.php">Start Chat</a> - <a href="./profile/profile.php">My Profile</a></h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <!-- Conversations are loaded here -->
+                <div class="direct-chat-messages">
+                  
+                <!-- Contacts are loaded here -->
+                <div class="">
+                  
+                  <ul class="contacts-list">
+                    <?php
 
-	$currentuser = $_SESSION['userid'];
+                      $currentuser = $_SESSION['userid'];
 
-	//echo $currentuser;
+                      //include('./message-select.php');
 
-	$public_account = 0;
+                      //$query = "SELECT messages.*, user.* FROM messages INNER JOIN user ON messages.readerid = user.id WHERE messages.senderid = '$currentuser' OR messages.readerid = '$currentuser' GROUP BY user.id";
 
-	$query = "SELECT * FROM user WHERE id <> '$currentuser' AND private_account = '$public_account'";
-	$result = mysqli_query($connect, $query);
+                      $query_chats = "SELECT chats.*, user.* FROM user INNER JOIN chats ON user.id = chats.user_id WHERE chats.current_user_id = '$currentuser' OR chats.user_id = '$currentuser' ORDER BY chats.chat_date DESC, chats.chat_time DESC";
+                      $result_chats = mysqli_query($connect, $query_chats);
 
-	if (mysqli_num_rows($result) > 0) {
-		while($rows = mysqli_fetch_assoc($result)) {
-			$verify_tick = $rows['verified'];
-			$profile_photo = $rows['profile_photo'];
-			$user_id_for_followers = $rows['id'];
+                      //$rows_before = mysqli_fetch_assoc($result_chats);
 
-		
-		if ($verify_tick == 1) {
-			
-			echo '
+                      if (mysqli_num_rows($result_chats) > 0) {
 
-			    <div>
-			        <p>
-				        <form method="GET" action="./read.php">
-					        <img src="./profile/' .$profile_photo. '" class="profile_user">
-					        <input type="hidden" name="user" value="' .$rows["id"]. '">
-					        <button>&nbsp;&nbsp;' .$rows["full_name"]. '
-					        </button>&nbsp;&nbsp;
-					        <img src="./photos/verify.jpg" class="verified"> 
-			            </form>
-			            <a href="./profile/profile.php?opid=' .$user_id_for_followers. '">Profile</a>
-			        </p>
-		        </div>
+                        while($rows_chats = mysqli_fetch_assoc($result_chats)) {
 
-			';
-		}
+                          $my_id = $rows_chats['current_user_id'];
+                          $your_id = $rows_chats['user_id'];
+                          //$user_id_two = $rows_chats['']
 
-		/*<div>
-			<p>
-				<form method="GET" action="./read.php">
-					<img src="./profile/<?php echo $profile_photo;?>" class="profile_user">
-					<button name="user" value="<?php echo $rows['id'];?>">&nbsp;&nbsp;<?php echo $rows['full_name'];?>
-					</button>&nbsp;&nbsp;
-					    <?php if($verify_tick == 1) {?><img src="./photos/verify.jpg" class="verified"> 
-			    </form>
-			</p>
-		</div>
-		    
-		<?php
-	}*/
+                          if ($my_id == $currentuser) {
+        
+                            $id_to_display = $your_id;
+                          } else {
 
-	//select and count number of followers
-		    $select_followers = "SELECT * FROM followers WHERE my_id = '$user_id_for_followers'";
-		    $select_followers_result = mysqli_query($connect, $select_followers);
-		    $count_followers = mysqli_num_rows($select_followers_result);
+                            $id_to_display = $my_id;
+                          }
 
-		    echo "
-		    <form method='GET' action='./friends.php'>
-		        <input type='hidden' name='friends' value='" .$user_id_for_followers. "'>
-		        <button>Followed By 
-		            <span style='color: blue;'>" .$count_followers. " User</span>
-		        </button>
-		    </form>";
+                    ?>
+                    <li>
+                      <a href="read.php?user=<?php echo $id_to_display;?>">
+                        <?php 
+                              //if ($id_to_display == $your_id) {
+                  
+                              $select_your_id = "SELECT * FROM user WHERE id = '$id_to_display'";
+                              $select_your_id_result = mysqli_query($connect, $select_your_id);
 
-		    ?>
-        <hr>
+                              if (mysqli_num_rows($select_your_id_result) > 0) {
+                    
+                                $select_your_id_rows = mysqli_fetch_assoc($select_your_id_result);
 
-        <?php
-}
-}
-}
+                                $verify_tick = $select_your_id_rows['verified'];
+                                $profile_photo = $select_your_id_rows['profile_photo'];
+                                $user_full_name = $select_your_id_rows['full_name'];
+                            
+                            ?>
 
-	?>
+                      <!--<img src="./profile/<?php //echo $profile_photo;?>" class="profile_user">&nbsp;<?php //echo $select_your_id_rows['full_name'];?>&nbsp; <?php //if($verify_tick == 1) {?><img src="./photos/verify.jpg" class="verified"></span><br>-->
 
-	<?php
+                      <img class="contacts-list-img" src="./profile/<?php echo $profile_photo;?>" alt="User Avatar">
 
-	if (isset($_GET['search'])) {
-		
-		$currentuser = $_SESSION['userid'];
+                        <div class="contacts-list-info">
+                          <span class="contacts-list-name">
 
-	//echo $currentuser;
+                            <!-- Name not displaying -->                          
+                            <?php echo $user_full_name;?>
 
-	$username_to_search = $_GET['search'];
+                            <?php
 
-	$query = "SELECT * FROM user WHERE id <> '$currentuser' AND username = '$username_to_search'";
-	$result = mysqli_query($connect, $query);
+                            //php code to display the last message send by either user
 
-	if (mysqli_num_rows($result) > 0) {
-		while($rows = mysqli_fetch_assoc($result)) {
-			$verify_tick = $rows['verified'];
-			$profile_photo = $rows['profile_photo'];
-			$user_id_for_followers = $rows['id'];
+                              $select_message_display = "SELECT messages.*, readmessages.* FROM readmessages INNER JOIN messages ON readmessages.messageid = messages.id WHERE readmessages.sender_id = '$currentuser' AND readmessages.reciever_id = '$id_to_display' OR readmessages.sender_id = '$id_to_display' AND readmessages.reciever_id = '$currentuser' ORDER BY readmessages.messageid DESC LIMIT 1";
+                              $select_message_display_result = mysqli_query($connect, $select_message_display);
 
-		
-		if ($verify_tick == 1) {
-			
-			echo '
+                              if (mysqli_num_rows($select_message_display_result) > 0) {
 
-			    <div>
-			        <p>
-				        <form method="GET" action="./read.php">
-					        <img src="./profile/' .$profile_photo. '" class="profile_user">
-					        <input type="hidden" name="user" value="' .$rows["id"]. '">
-					        <button>&nbsp;&nbsp;' .$rows["full_name"]. '
-					        </button>&nbsp;&nbsp;
-					        <img src="./photos/verify.jpg" class="verified"> 
-			            </form>
-			            <a href="./profile/profile.php?opid=' .$user_id_for_followers. '">Profile</a>
-			        </p>
-		        </div>
+                                //$count_unread = mysqli_num_rows($select_message_display_result);
+                  
+                                while ($rows_messages_read = mysqli_fetch_assoc($select_message_display_result)) {
 
-			';
-		}
+                                  $select_time_to_display = $rows_messages_read['time'];
+                                  $select_date_to_display = $rows_messages_read['date'];
+                                  $select_user_to_display = $rows_messages_read['senderid'];
+                    
+                            ?>
+                            <small class="contacts-list-date float-right"><!--2/23/2015--><?php echo $select_time_to_display. " - " .$select_date_to_display;?>
+                            </small>
+                          </span>
+                          
+                          <span class="contacts-list-msg">
 
-		/*<div>
-			<p>
-				<form method="GET" action="./read.php">
-					<img src="./profile/<?php echo $profile_photo;?>" class="profile_user">
-					<button name="user" value="<?php echo $rows['id'];?>">&nbsp;&nbsp;<?php echo $rows['full_name'];?>
-					</button>&nbsp;&nbsp;
-					    <?php if($verify_tick == 1) {?><img src="./photos/verify.jpg" class="verified"> 
-			    </form>
-			</p>
-		</div>
-		    
-		<?php
-	}*/
 
-	//select and count number of followers
-		    $select_followers = "SELECT * FROM followers WHERE my_id = '$user_id_for_followers'";
-		    $select_followers_result = mysqli_query($connect, $select_followers);
-		    $count_followers = mysqli_num_rows($select_followers_result);
+                            <?php
+                              //if sender is me, then display this, else don't display to reciever (changes to make)
+                              //count the number of unread messages and display how they are
+                              //capitalize first letters for each sentense when writing a message
+                              //don't display badge and single and double ticks to the sender
+                                
 
-		    echo "
-		    <form method='GET' action='./friends.php'>
-		        <input type='hidden' name='friends' value='" .$user_id_for_followers. "'>
-		        <button>Followed By 
-		            <span style='color: blue;'>" .$count_followers. " User</span>
-		        </button>
-		    </form>";
+                                if ($id_to_display == $select_user_to_display) {
+  
+                                  if ($rows_messages_read['status'] == 0) {
 
-		    ?>
-        <hr>
+                                  $zero = 0;
 
-        <?php
-}
-} else {
+                                  $select_message_display_count = "SELECT * FROM readmessages WHERE sender_id = '$currentuser' AND reciever_id = '$id_to_display' AND status = '$zero' OR sender_id = '$id_to_display' AND reciever_id = '$currentuser' AND status = '$zero'";
+                                  $select_message_display_result_count = mysqli_query($connect, $select_message_display_count);
 
-	echo "No user with that username!";
-}
-	}
-	?>
+                                  $count_unread = mysqli_num_rows($select_message_display_result_count);
+                          
+                                  echo "&nbsp;&nbsp; <span style='overflow: hidden; text-overflow: ellipsis; white-space: nowrap; width: 90px;' id='message'>" .$rows_messages_read['message']. "</span> <span class='badge badge-info right' style='float: right;'>" .$count_unread. "</span>";
+                                } elseif ($rows_messages_read['status'] == 1) {
+                          
+                                  echo "&nbsp;&nbsp; <span style='overflow: hidden; text-overflow: ellipsis; white-space: nowrap; width: 90px;' id='message'>" .$rows_messages_read['message']. "</span>" ;
+                                }
+
+                                } else {
+
+                                  if ($rows_messages_read['status'] == 0) {
+
+                                  $zero = 0;
+
+                                  $select_message_display_count = "SELECT * FROM readmessages WHERE sender_id = '$currentuser' AND reciever_id = '$id_to_display' AND status = '$zero' OR sender_id = '$id_to_display' AND reciever_id = '$currentuser' AND status = '$zero'";
+                                  $select_message_display_result_count = mysqli_query($connect, $select_message_display_count);
+
+                                  $count_unread = mysqli_num_rows($select_message_display_result_count);
+                          
+                                  echo "&nbsp;&nbsp;&#x2713; <span style='overflow: hidden; text-overflow: ellipsis; white-space: nowrap; width: 90px;' id='message'>" .$rows_messages_read['message']. "</span>";
+                                } elseif ($rows_messages_read['status'] == 1) {
+                          
+                                  echo "&nbsp;&nbsp; <span style='color: blue;'>&#x2713;&#x2713;<?span> <span style='overflow: hidden; text-overflow: ellipsis; white-space: nowrap; width: 90px;' id='message'>" .$rows_messages_read['message']. "</span>" ;
+                                }
+                                }
+                            ?>
+                            
+                            <?php
+                                  }
+                                }
+                            ?>
+
+                      <?php
+                    }
+                  }
+                 //}
+                 ?>
+                        
+                            
+
+                          </span>
+                        </div>
+                        <!-- /.contacts-list-info -->
+                      </a>
+                    </li>
+                            <?php
+                                }
+                              //}
+
+                            ?>
+                    <!-- End Contact Item -->
+
+                  </ul>
+                  <!-- /.contacts-list -->
+
+                </div>
+                <!-- /.direct-chat-pane -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                
+              </div>
+              <!-- /.card-footer-->
+            </div>
+            <!--/.direct-chat -->
+          </section>
+          <!-- /.Left col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<script>
+  var mes = document.getElementById('message').innerHTML;
+  var truncated = mes.substring(0, 50) + ' ';
+  document.getElementById('message').innerHTML = truncated;
+</script>
+
+<!-- jQuery -->
+<script src="./../plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="./../plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="./../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="./../plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="./../plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="./../plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="./../plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="./../plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="./../plugins/moment/moment.min.js"></script>
+<script src="./../plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="./../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="./../plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="./../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="./../dist/js/adminlte.js"></script>
 </body>
 </html>
