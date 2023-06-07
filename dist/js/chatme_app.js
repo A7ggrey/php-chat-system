f83ac258e8e9c1c2316e8af2ba3b2b7bbeb98471
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-06-07 20:43:11 +0300

    Update chatme_app.js

--- dist/js/chatme_app.js
+++ dist/js/chatme_app.js
@@ -1,9 +1,14 @@
+//email and password authentication
 function loginForm() {}
 
+//full name, email, username, password and confirm password authentication
 function signupForm() {}
 
+//
 function updateProfile() {}
 
+//
 function updateAboutMe() {}
 
+//message input authentication
 function sendMessage() {}
