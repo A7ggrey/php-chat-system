c92f8a152a4bc36cada173651d26d528d18a218b
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-06-09 12:19:48 +0300

    Update chatme_app.js

--- dist/js/chatme_app.js
+++ dist/js/chatme_app.js
@@ -5,7 +5,22 @@ form validation of all forms contained in this project chat me
 */
 
 //email and password authentication
-function loginForm() {}
+function loginForm() {
+	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+
+	if (email == '' || email == null) {
+
+		alert('Email is required!');
+		return false;
+	}
+
+	if (password == '' || password == null) {
+
+		alert('Password is required!');
+		return false;
+	}
+}
 
 //full name, email, username, password and confirm password authentication
 function signupForm() {}
