54d7f209e59ae99dc52459efd1014716c80e2b96
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-06-15 20:19:34 +0300

    Update chats.php

--- chatme/chats.php
+++ chatme/chats.php
@@ -17,7 +17,7 @@ include('./../database/database.php');
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Chat Me | Chats</title>
+  <title>Chat Me - Chats</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
